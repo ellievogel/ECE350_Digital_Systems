c71b28b9e2d501ef67ee42ca8ee3e295a14e6fb1
Ellie Vogel <152567958+ellievogel@users.noreply.github.com>
2025-04-16 16:51:10 -0400

    Update controller.v

--- final_project/controller.v
+++ final_project/controller.v
@@ -1,24 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 04/15/2025 05:27:58 PM
-// Design Name: 
-// Module Name: claw_movement
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: Controls a 4-input stepper motor with simple step sequencing
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Edited
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 module claw_movement(
     input CLK100MHZ,
     input input_motor_sig,
@@ -28,7 +7,7 @@ module claw_movement(
     reg [1:0] state = 0;
     reg [23:0] delay_counter = 0;
 
-    parameter MAX_COUNT = 24'd8_000_000; // ~100ms delay at 100MHz
+    parameter MAX_COUNT = 24'd1_000_000; // ~100ms delay at 100MHz
 
     always @(posedge CLK100MHZ) begin
         if (delay_counter == MAX_COUNT) begin
@@ -49,21 +28,20 @@ module claw_movement(
     always @(*) begin
         case (state)
             2'd0: begin
-                jb1 = 1; jb2 = 0; jb3 = 0; jb4 = 0;
+                jb1 = 1; jb2 = 0; jb3 = 0; jb4 = 1;
             end
             2'd1: begin
-                jb1 = 0; jb2 = 1; jb3 = 0; jb4 = 0;
+                jb1 = 1; jb2 = 0; jb3 = 1; jb4 = 0;
             end
             2'd2: begin
-                jb1 = 0; jb2 = 0; jb3 = 1; jb4 = 0;
+                jb1 = 0; jb2 = 1; jb3 = 1; jb4 = 0;
             end
             2'd3: begin
-                jb1 = 0; jb2 = 0; jb3 = 0; jb4 = 1;
+                jb1 = 0; jb2 = 1; jb3 = 0; jb4 = 1;
             end
-            default: begin
-                jb1 = 0; jb2 = 0; jb3 = 0; jb4 = 0;
-            end
+//            default: begin
+//                jb1 = 0; jb2 = 0; jb3 = 0; jb4 = 0;
+//            end
         endcase
     end
-
 endmodule
